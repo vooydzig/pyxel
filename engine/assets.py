5fccce3efed01e2bab115b5ef76bc9e9e4e0a469
TomekWójcik <tomasz.wojcik@tylko.com>
2025-10-11 21:16:10 +0200

    added font assets

--- engine/assets.py
+++ engine/assets.py
@@ -1,10 +1,12 @@
 import os
-
+import pygame
 from pygame import image
 
 
 class AssetManager:
     IMAGES = ['.png', '.jpg', '.jpeg', '.bmp', '.gif']
+    FONTS = ['.ttf', '.otf']
+    FONT_SIZES = [12, 18, 24, 32, 48, 64]
 
     def __init__(self, root_dir):
         self.root_dir = root_dir
@@ -15,15 +17,24 @@ class AssetManager:
         }
 
     def initialize(self):
+        self.assets['font']['default'] = pygame.font.SysFont('Arial', 18)
         for dirpath, _, filenames in os.walk(self.root_dir):
             for filename in filenames:
                 filepath = os.path.join(dirpath, filename)
                 name, ext = os.path.splitext(filename)
                 if ext.lower() in self.IMAGES:
-                    self.load_asset(name, 'image', filepath)
+                    self._load_image(name, filepath)
+                if ext.lower() in self.FONTS:
+                    for size in self.FONT_SIZES:
+                        font_key = f"{name}_{size}"
+                        self._load_font(font_key, filepath, size)
+                    self._load_font(name, filepath, 18)  # Default size
 
-    def load_asset(self, name, asset_type, path):
-        self.assets[asset_type][name] = image.load(path)
+    def _load_image(self, name, path):
+        self.assets['image'][name] = image.load(path)
+
+    def _load_font(self, name, font_name, font_size):
+        self.assets['font'][name] = pygame.font.Font(font_name, font_size)
 
     def get_asset(self, asset_type, name):
         return self.assets.get(asset_type, {}).get(name)
