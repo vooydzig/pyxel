d62cf539bdc00acc57d6723bd14142bbec7291fe
vooydzig <vooydzig@gmail.com>
2026-04-18 19:47:16 +0200

    added turn logic to warpspeed effect

--- warp/app.py
+++ warp/app.py
@@ -8,18 +8,42 @@ from entity import Star
 
 class WarpspeedApp(App):
     STARS_COUNT = 1000
+    TURN_ACCELERATION = 0.1
+    TURN_DAMPING = 0.86
+    EMITTER_DEPTH = 10
+    EMITTER_DRIFT = 1.0
 
     def initialize(self):
         super().initialize()
+        self.turn_velocity = pygame.Vector2()
+        self.turn_delta = pygame.Vector2()
+        self.emitter_center = pygame.Vector2()
         for i in range(self.STARS_COUNT):
             self.entities.append(Star(f'star_{i}', self.get_random_position()))
+        self.input.mouse.position = self.screen_size / 2
+        self.last_mouse_position = pygame.Vector2(self.input.mouse.position)
+
+    def _update_input(self):
+        super()._update_input()
+        mouse_delta = self.input.mouse.position - self.last_mouse_position
+        turn_impulse = pygame.Vector2(
+            mouse_delta.x * self.TURN_ACCELERATION,
+            mouse_delta.y * self.TURN_ACCELERATION
+        )
+        self.turn_velocity += turn_impulse
+        self.turn_velocity *= self.TURN_DAMPING
+        self.turn_delta = pygame.Vector2(self.turn_velocity)
+        self.emitter_center += self.turn_delta * self.EMITTER_DEPTH * self.EMITTER_DRIFT
+        self.last_mouse_position = pygame.Vector2(self.input.mouse.position)
 
     def _update_entities(self):
+        for star in self.entities:
+            star.turn_delta = self.turn_delta
         super()._update_entities()
         for star in self.entities:
             if star.position.z < 1:
                 star.position = self.get_random_position()
-                star.pz = star.position.z
+                star.previous_position = pygame.Vector3(star.position)
 
     def get_random_position(self):
         return pygame.Vector3(
--- warp/entity.py
+++ warp/entity.py
@@ -4,26 +4,34 @@ from core.entity import Entity
 
 
 class Star(Entity):
-    SPEED = 0.5
+    SPEED = 0.1
 
     def __init__(self, name, position):
         super().__init__(name, None, position)
-        self.pz = self.position.z
+        self.previous_position = pygame.Vector3(self.position)
         self.size = 4
+        self.turn_delta = pygame.Vector2()
 
     def render(self, surface):
         surface_size = pygame.math.Vector2(surface.get_size())
         coords_2d = self.to_screen_coords(self.position, self.position.z, surface_size)
-        prev_coords_2d = self.to_screen_coords(self.position, self.pz, surface_size)
+        prev_coords_2d = self.to_screen_coords(
+            self.previous_position,
+            self.previous_position.z,
+            surface_size
+        )
         pygame.draw.line(surface, (255, 255, 255), coords_2d, prev_coords_2d, self.size)
-        self.pz = self.position.z
 
     def update(self, dt, input_manager=None):
+        self.previous_position = pygame.Vector3(self.position)
         speed = self.SPEED
         if input_manager.is_mouse_held(pygame.BUTTON_LEFT):
             speed *= 2
         elif input_manager.is_mouse_held(pygame.BUTTON_RIGHT):
             speed /= 2
+
+        self.position.x += self.turn_delta.x * self.position.z
+        self.position.y += self.turn_delta.y * self.position.z
         self.position.z -= speed
 
     def to_screen_coords(self, coords_3d, camera_distance, surface_size):
