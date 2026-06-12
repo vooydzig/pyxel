81d9863206f80110aa2501656ec66735446a5338
vooydzig <vooydzig@gmail.com>
2026-06-13 00:18:10 +0200

    abstracted parallax backgrounds a bit

--- parallax/app.py
+++ parallax/app.py
@@ -1,37 +1,12 @@
-import pygame
-
 from core.app import App
-from background import LayeredBackground, BackgroundLayer, ParallaxLayer
+from parallax.background import MountainRange
 
 
 class ParallaxApp(App):
 
     def initialize(self):
         super().initialize()
-        background = LayeredBackground()
-        background.add_layers([
-            BackgroundLayer.create_fit_to_screen(
-                f'layer_1',
-                self.asset_manager.get_asset('image', 'Bckg'),
-                pygame.Vector2(0, 0),
-                self.renderer.canvas_size,
-            ),
-            ParallaxLayer.create_fit_to_screen(
-                f'layer_2',
-                self.asset_manager.get_asset('image', 'Cloud'),
-                pygame.Vector2(0,0),
-                self.renderer.canvas_size,
-                0.2
-            ),
-            ParallaxLayer.create_fit_to_screen(
-                f'layer_3',
-                self.asset_manager.get_asset('image', 'Mountain_2'),
-                pygame.Vector2(0, 0),
-                self.renderer.canvas_size,
-                0.1
-            )
-        ])
-        self.renderer.background = background
+        self.renderer.background = MountainRange(self.asset_manager, self.renderer.canvas_size)
 
     def _update_entities(self):
         self.renderer.background.update(self.clock.get_time(), self.input)
--- parallax/background.py
+++ parallax/background.py
@@ -1,90 +1,30 @@
 import pygame
 
-from core import renderer
-from core.entity import Entity
+from core.background import LayeredBackground, BackgroundLayer, ParallaxLayer
 
 
-class LayeredBackground(renderer.Background):
-    def __init__(self):
-        self.layers = []
-
-    def add_layers(self, layers: list):
-        self.layers.extend(layers)
-
-    def render(self, screen: pygame.Surface):
-        for layer in self.layers:
-            layer.render(screen)
-
-    def update(self, dt, input_manager=None):
-        for layer in self.layers:
-            layer.update(dt, input_manager)
-
-
-class BackgroundLayer(Entity):
-    def __init__(self, name, asset, position=pygame.Vector2(), size=pygame.Vector2(64, 64)):
-        self.size = size
-        super().__init__(name, pygame.transform.scale(asset, self.size), position)
-
-    def render(self, surface):
-        surface.blit(self.asset, self.position)
-
-    @classmethod
-    def create_fit_to_screen(cls, name, image, position, screen_size):
-        image_size = pygame.Vector2(image.get_size())
-        if image_size.x > image_size.y:
-            scale_factor = screen_size.x / image_size.x
-        else:
-            scale_factor = screen_size.y / image_size.y
-
-        return cls(
-            name, image, position, size=image_size * scale_factor
-        )
-
-class ParallaxLayer(BackgroundLayer):
-    ACTIONS = {
-        pygame.K_a: 'move_left',
-        pygame.K_d: 'move_right',
-    }
-
-    def __init__(self, speed, *args, **kwargs):
-        self.speed = speed
-        self.velocity = pygame.Vector2()
-        self.blit_position_x = 0
-        super().__init__(*args, **kwargs)
-
-
-    @classmethod
-    def create_fit_to_screen(cls, name, image, position, screen_size, speed):
-        image_size = pygame.Vector2(image.get_size())
-        if image_size.x > image_size.y:
-            scale_factor = screen_size.x / image_size.x
-        else:
-            scale_factor = screen_size.y / image_size.y
-
-        new_size = image_size * scale_factor
-        return cls(speed, name, image, position, new_size)
-
-    def update(self, dt, input_manager=None):
-        if input_manager:
-            self.velocity.x = 0
-            self.velocity.y = 0
-            if input_manager.is_key_held(pygame.K_a):
-                self.velocity.x += 1
-
-            if input_manager.is_key_held(pygame.K_d):
-                self.velocity.x += -1
-
-        if self.velocity.length() > 0:
-            self.velocity = self.velocity.normalize()
-        self.position += self.velocity * self.speed * dt
-
-    def render(self, surface):
-        if self.position.x >= surface.get_size()[0]:
-            self.position.x = 0
-        elif self.position.x <= -surface.get_size()[0]:
-            self.position.x = 0
-        offset = pygame.Vector2(self.asset.get_size()[0], 0)
-        surface.blit(self.asset, self.position-offset)
-        surface.blit(self.asset, self.position)
-        surface.blit(self.asset, self.position+offset)
-
+class MountainRange(LayeredBackground):
+    def __init__(self, asset_manager, background_size):
+        super().__init__()
+        self.add_layers([
+            BackgroundLayer.create_fit_to_screen(
+                f'layer_1',
+                asset_manager.get_asset('image', 'Bckg'),
+                pygame.Vector2(0, 0),
+                background_size,
+            ),
+            ParallaxLayer.create_fit_to_screen(
+                f'layer_2',
+                asset_manager.get_asset('image', 'Cloud'),
+                pygame.Vector2(0, 0),
+                background_size,
+                0.2
+            ),
+            ParallaxLayer.create_fit_to_screen(
+                f'layer_3',
+                asset_manager.get_asset('image', 'Mountain_2'),
+                pygame.Vector2(0, 0),
+                background_size,
+                0.1
+            )
+        ])
