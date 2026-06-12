fcd8f6752ae92a68534323eed21f6d075efb5e8c
vooydzig <vooydzig@gmail.com>
2026-06-13 00:33:09 +0200

    better mountain parallax background

--- parallax/background.py
+++ parallax/background.py
@@ -9,22 +9,43 @@ class MountainRange(LayeredBackground):
         self.add_layers([
             BackgroundLayer.create_fit_to_screen(
                 f'layer_1',
-                asset_manager.get_asset('image', 'Bckg'),
+                asset_manager.get_asset('image', 'sky'),
                 pygame.Vector2(0, 0),
                 background_size,
             ),
             ParallaxLayer.create_fit_to_screen(
                 f'layer_2',
-                asset_manager.get_asset('image', 'Cloud'),
+                asset_manager.get_asset('image', 'clouds_bg'),
+                pygame.Vector2(0, 0),
+                background_size,
+                0.1
+            ),
+            ParallaxLayer.create_fit_to_screen(
+                f'layer_5',
+                asset_manager.get_asset('image', 'glacial_mountains'),
                 pygame.Vector2(0, 0),
                 background_size,
                 0.2
             ),
             ParallaxLayer.create_fit_to_screen(
                 f'layer_3',
-                asset_manager.get_asset('image', 'Mountain_2'),
+                asset_manager.get_asset('image', 'cloud_lonely'),
                 pygame.Vector2(0, 0),
                 background_size,
-                0.1
-            )
+                0.5
+            ),
+            ParallaxLayer.create_fit_to_screen(
+                f'layer_6',
+                asset_manager.get_asset('image', 'clouds_mg_1'),
+                pygame.Vector2(0, 0),
+                background_size,
+                0.3
+            ),
+            ParallaxLayer.create_fit_to_screen(
+                f'layer_7',
+                asset_manager.get_asset('image', 'clouds_mg_2'),
+                pygame.Vector2(0, 0),
+                background_size,
+                0.25
+            ),
         ])
